8960731a896b656edebb5eeb6545ddb2d13596eb
2010101289 <danielodf2.aluno@unipampa.edu.br>
2022-10-07 16:47:32 -0300

    corrigindo a movimentação do personagem

--- Scenes/PlayerScript.gd
+++ Scenes/PlayerScript.gd
@@ -5,21 +5,33 @@ var speed = 200;
 
 func _process(delta):
 	
+	var move_direction = int(Input.is_action_pressed("ui_right")) - int (Input.is_action_pressed("ui_left"))
+	move_and_slide(motion,Vector2.UP)
+	
 	if is_on_floor():
 		print("chao")
 		if  Input.is_action_pressed("ui_right") or  Input.is_action_pressed("ui_left"):
 			$AnimatedSprite.play("Run")
+			
+		if Input.is_action_pressed("ui_right"):
+			motion.x = speed;
+			$AnimatedSprite.flip_h = false
+			
+		elif Input.is_action_pressed("ui_left"):
+			motion.x = -speed
+			$AnimatedSprite.flip_h = true
+				
+		elif  Input.is_action_pressed("ui_up"):
+			$AnimatedSprite.play("Jump")
+			motion.y = -200
+		else:
+			motion.x = 0;
+			$AnimatedSprite.play("Idle")
+			move_and_slide(motion,Vector2.UP)
+			
 	else:
-		motion.y += 10
-		
+		motion.y += 10	
 		print("ar")
 		
-	if Input.is_action_pressed("ui_right"):
-		motion.x = speed;
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -speed
-	else:
-		motion.x = 0;
-		
-		move_and_slide(motion,Vector2.UP)
+	
 
